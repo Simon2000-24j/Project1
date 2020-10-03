4d4ac0f46ebf5731158cc38c2704a35d61995a94
Simon2000-24j <71288933+Simon2000-24j@users.noreply.github.com>
2020-10-03 21:47:34 +0300

    Update branching.py

--- branching.py
+++ branching.py
@@ -1,40 +1,23 @@
+city = input('Введите город: ')
 cities = ['Москва', 'Париж', 'Лондон']
-
-new_city = 'Нью-Йорк'
-
-if new_city not in cities:
-    cities.append(new_city)
-    print('Город добавлен в список')
-else:
-    print('Город не добавлен в список')
 
 users = [{'name': 'Иван', 'age': 35},
          {'name': 'Мария', 'age': 22},
-         {'name': 'Софья', 'age': 20}]
-
-new_user = {'name': 'Пётр', 'age': 25}
-
-if new_user not in users:
-    users.append(new_user)
-    print('Пользователь добавлен в список')
-else:
-    print('Пользователь не добавлен в список')
+         {'name': 'Соня', 'age': 20}]
 
 tourists = [{'user': users[0], 'city': cities[0]},
             {'user': users[1], 'city': cities[1]},
             {'user': users[2], 'city': cities[2]}]
 
-new_tourist = {'user': users[3], 'city': cities[3]}
-
-if new_tourist not in tourists:
-    tourists.append(new_tourist)
-    print('Турист добавлен в список')
-else:
-    print('Турист не добавлен в список')
-
-city = input('Введите город: ')
-tourist = input('Введите имя туриста: ')
-age = input('Введите возраст: ')
 
 
-print('Турист ', tourist, 'возраст', age, '. ', 'Посетил город', city)
+if city == 'ПаРиж':
+     print (f"Турист {users[1]['name']} возраст {users[1]['age']}. Посетил город {cities[1]}") 
+if city == 'париЖ':
+     print (f"Турист {users[1]['name']} возраст {users[1]['age']}. Посетил город {cities[1]}")      
+if cities[0]== city :
+     print(f"Турист {users[0]['name']} возраст {users[0]['age']}. Посетил город {cities[0]}")
+if cities[1] == city : 
+     print (f"Турист {users[1]['name']} возраст {users[1]['age']}. Посетил город {cities[1]}")  
+if cities[2] == city :  
+     print (f"Турист {users[2]['name']} возраст {users[2]['age']}. Посетил город {cities[2]}")
