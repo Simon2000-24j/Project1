c5a4979b8da06b615a06d72299a6ce9811fc4771
Simon2000-24j <71288933+Simon2000-24j@users.noreply.github.com>
2020-10-03 21:50:59 +0300

    Update absolute.py

--- absolute.py
+++ absolute.py
@@ -1,15 +1,18 @@
-def get_absolute_url(num1, *args):
-    print(args)
-    for number in args:
-        print(number)
-
-get_absolute_url(2, 5, 6, 7)
-
-# или
-
-def get_absolute_url(num1, **kwargs):
+def get_absolute_url(abs_url, *args, **kwargs):
+    string_url = abs_url
+    for arguments in args:
+        string_url += '/' + arguments
     for k, v in kwargs.items():
-        print(k, v)
+        string_url += '?' + k + '=' + v + '&'
+    #в строке последний знак получился "&" обрезаем его
+    string_url = string_url[:-1]
+    #в строке получились пары '&?', а должно быть '&' делаем замену шаблона для этих пар
+    string_url = string_url.replace('&?', '&')
+    return string_url
 
 
-get_absolute_url(2, a = 5, b = 6, c = 7)
+#ЭТО ПРИМЕРЫ ИСПОЛЬЗОВАНИЯ.
+#ЛИБО НАПИСАТЬ ВЫВОД ФУНКЦИЙ СО СВОИМИ ПАРАМЕТРАМИ
+print(get_absolute_url('www.yandex.ru', 'posts', 'news', id='24', author='admin'))
+print(get_absolute_url('www.google.com', 'images', id='24', category='auto', color='azure', size='small'))
+print(get_absolute_url('www.yandex.ru', 'posts', 'news', 'images', id='24', author='admin',  category='auto', color='azure', size='small'))
