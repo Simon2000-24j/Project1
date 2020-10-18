08a91a9cc67c5b6af1165776494c74c34517cde6
Simon2000-24j <71288933+Simon2000-24j@users.noreply.github.com>
2020-10-18 13:18:12 +0300

    Update pizza.py

--- pizza.py
+++ pizza.py
@@ -1,16 +1,28 @@
-# Создаем продукты с указанием названия, калорийности продукта и его себестоимости
-dough_product = Product('Тесто', 200, 20)
-tomato_product = Product('Помидор', 100, 50)
-cheese_product = Product('Сыр', 100, 120)
+class Product:
+    def __init__(self, title, price, calories):
+        self.title = title
+        self.price = price
+        self.calories = calories
+        dough_product = Product('Тесто', 200, 20)
+        tomato_product = Product('Помидор', 100, 50)
+        cheese_product = Product('Сыр', 100, 120)
+        sausage_product = Product('Колбаса', 200, 70)
+        pizza_product = Product('Пепперони', 600, 260)
 
-# Из продуктов создаем ингредиенты. Для каждого ингредиента указываем продукт, 
-# из которого он состоит и вес продукта
-dough_ingredient = Ingredient(dough_product, 100)
-tomato_ingredient = Ingredient(tomato_product, 100)
-cheese_ingredient = Ingredient(cheese_product, 100)
 
-# Из ингредиентов создаем пиццу
-pizza_margarita = Pizza('Маргарита', [dough_ingredient, tomato_ingredient, cheese_ingredient])
+class Ingredient:
+    def __init__(self, weight):
+        self.price = weight
+        dough_ingredient = Ingredient(dough_product, 100)
+        tomato_ingredient = Ingredient(tomato_product, 100)
+        cheese_ingredient = Ingredient(cheese_product, 100)
+        sausage_ingredient = Ingredient(sausage_product, 100)
+        pizza_ingredient = Ingredient(pizza_product, 400)
 
-# Выводим экземпляр пиццы
-print(pizza_margarita)
+class Pizza:
+    def __init__(self, ingredients):
+        self.ingredients = ingredients
+        pizza_pepperoni = Pizza('Пепперони', [dough_ingredient, tomato_ingredient, cheese_ingredient, sausage_ingredient])
+        
+print('Пепперони', 260, 'kkal', 600, 'руб')
+
